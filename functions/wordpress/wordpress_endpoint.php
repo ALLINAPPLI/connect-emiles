f7fd5c331554d7e4582dd3f5d80fffbe97d4ab47
dewy <dewymercerais@gmail.com>
2023-10-26 10:40:15 +0200

    modification de l'erreur en retour + modification de l'affichage de la route dans le panneau option du plugin

--- functions/wordpress/wordpress_endpoint.php
+++ functions/wordpress/wordpress_endpoint.php
@@ -8,19 +8,28 @@ add_action('rest_api_init', function(){
         'callback' => 'connect_partnersite_return_json',
         'permission_callback' => function( \WP_REST_Request $request ) {
             if($request->get_header('host') !== get_option('url_partenaire')) {
-                return new \WP_Error( 'url_partenaire_invalid', 'l\'url partenaire ne corresponds pas au paramétrage du plugin', array( 'status' => 403 ) );
+                return new \WP_Error(
+                  'url_partenaire_invalid',
+                  'l\'url partenaire ne corresponds pas au paramétrage du plugin',
+                  array( 'status' => 403 )
+                );
+                
             }
             return true;
         },
         'args' => [
           'token' => [
-            'validate_callback' => function ( $value, \WP_REST_Request $request, $key ) {
+            /*'validate_callback' => function ( $value, \WP_REST_Request $request, $key ) {
                 if ( ! wp_is_uuid( $value ) ) {
-                    return new \WP_Error( 'uuid_invalid', 'l\'uuid de l\'utilisateur est invalide', array( 'status' => 400 ) );
+                    return new \WP_Error(
+                      'uuid_invalid',
+                      'l\'uuid de l\'utilisateur est invalide',
+                      array( 'status' => 400 )
+                    );
                 }
         
                 return true;
-            },
+            },*/
             'sanitize_callback' => function( $value ) {
                 return trim( $value );
             },
@@ -32,6 +41,10 @@ add_action('rest_api_init', function(){
 
 function connect_partnersite_return_json( $request ) {
     $uuidUser = $request['token'];
+    if ( !empty($uuidUser) && ! wp_is_uuid( $uuidUser ) ) {
+        return ['error' => 'l\'uuid de l\'utilisateur est invalide'];
+    }
+    
     if(!empty($uuidUser)) {
         $userQuery = get_users(array(
           'meta_key' => 'secure_id',
@@ -46,7 +59,8 @@ function connect_partnersite_return_json( $request ) {
           'email' => $userData->user_email,
         ];
     } else {
-        return new WP_Error( 'empty_token', esc_html__( 'Token vide' ), array( 'status' => 403 ) );
+        //return new WP_Error( 'empty_token', esc_html__( 'Token vide' ), array( 'status' => 403 ) );
+        return ['error' => 'Token vide'];
     }
     
     return $responseCustomEndpoint;
