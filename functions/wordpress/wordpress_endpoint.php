6556f768dd3b4b7f02637934f7b5e8305ff737cd
dewy <dewymercerais@gmail.com>
2024-03-26 20:06:50 +0100

    Ajout d'une route pour la nouvelle connexion airalpha et euralpha

--- functions/wordpress/wordpress_endpoint.php
+++ functions/wordpress/wordpress_endpoint.php
@@ -1,11 +1,9 @@
 <?php
-add_action('rest_api_init', function(){
-   register_rest_route(
-     'emiles/v1',
-     '/endpoint/data',
-     array(
-        'methods' => 'GET',
-        'callback' => 'connect_partnersite_return_json',
+  
+  add_action('rest_api_init', function () {
+    register_rest_route('emiles/v1', '/endpoint/data', [
+        'methods'           => 'GET',
+        'callback'          => 'connect_partnersite_return_json',
         /*'permission_callback' => function( \WP_REST_Request $request ) {
             if($request->get_header('host') !== get_option('url_partenaire')) {
                 return new \WP_Error(
@@ -16,8 +14,7 @@ add_action('rest_api_init', function(){
                 
             }
             return true;
-        },*/
-        'args' => [
+        },*/ 'args' => [
           'token' => [
             /*'validate_callback' => function ( $value, \WP_REST_Request $request, $key ) {
                 if ( ! wp_is_uuid( $value ) ) {
@@ -29,39 +26,66 @@ add_action('rest_api_init', function(){
                 }
         
                 return true;
-            },*/
-            'sanitize_callback' => function( $value ) {
-                return trim( $value );
+            },*/ 'sanitize_callback' => function ($value) {
+              return trim($value);
             },
-          ]
+          ],
         ],
-     )
-   );
-});
-
-function connect_partnersite_return_json( $request ) {
+      ]);
+  
+    register_rest_route('emiles/v1', '/endpoint/alpha', [
+      'methods'           => 'POST',
+      'callback'          => 'connect_partnersite_alpha_return_json',
+      'permission_callback' => function( \WP_REST_Request $request ) {
+        //var_dump($request->get_header('x_forwarded_for'));
+        if($request->get_header('x_forwarded_for') !== '::1') {
+          return new \WP_Error(
+            'ip_invalide',
+            'l\'adresse ip est invalide !',
+            array( 'status' => 403 )
+          );
+        
+        }
+        return true;
+      },
+      'args' => [
+      
+      ],
+    ]);
+  });
+  
+  function connect_partnersite_return_json($request)
+  {
     $uuidUser = $request['token'];
-    if ( !empty($uuidUser) && ! wp_is_uuid( $uuidUser ) ) {
-        return ['error' => 'l\'uuid de l\'utilisateur est invalide'];
+    if (!empty($uuidUser) && !wp_is_uuid($uuidUser)) {
+      return ['error' => 'l\'uuid de l\'utilisateur est invalide'];
     }
     
-    if(!empty($uuidUser)) {
-        $userQuery = get_users(array(
-          'meta_key' => 'secure_id',
-          'meta_value' => $uuidUser
-        ));
-    
-        $userData = new WP_User( $userQuery[0]->data->ID );
-    
-        $responseCustomEndpoint = [
-          'firstname' => $userData->first_name,
-          'lastname' => $userData->last_name,
-          'email' => $userData->user_email,
-        ];
+    if (!empty($uuidUser)) {
+      $userQuery = get_users([
+        'meta_key'   => 'secure_id',
+        'meta_value' => $uuidUser,
+      ]);
+      
+      $userData = new WP_User($userQuery[0]->data->ID);
+      
+      $responseCustomEndpoint = [
+        'firstname' => $userData->first_name,
+        'lastname'  => $userData->last_name,
+        'email'     => $userData->user_email,
+      ];
     } else {
-        //return new WP_Error( 'empty_token', esc_html__( 'Token vide' ), array( 'status' => 403 ) );
-        return ['error' => 'Token vide'];
+      //return new WP_Error( 'empty_token', esc_html__( 'Token vide' ), array( 'status' => 403 ) );
+      return ['error' => 'Token vide'];
     }
     
     return $responseCustomEndpoint;
-}
+  }
+  
+  function connect_partnersite_alpha_return_json($request) {
+    //var_dump($request);
+    $responseCustomEndpointAlpha = [
+      'reponse' => 'OK'
+    ];
+    return $responseCustomEndpointAlpha;
+  }
